3bb864cdee4188813b56dfaf557f30e4b48a8a5a
doiko123 <ayanottyz@gmail.com>
2021-03-29 22:28:40 +0900

    rails db:migrate

--- db/migrate/20210329130630_create_products.rb
+++ db/migrate/20210329130630_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
-    create_table :products do |t|
-      t.string :stripe_id
-      t.string :name
+    create_table :products, comment:'商品' do |t|
+      t.string :stripe_id, null: false, comment: 'StripeのID'
+      t.string :name, null: false, comment: '商品名'
 
       t.timestamps
     end
