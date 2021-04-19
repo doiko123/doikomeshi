26a0f9cbecd00026f87fb986af589be1a60fadbd
doiko123 <ayanottyz@gmail.com>
2021-04-19 23:11:21 +0900

    モデルのバリデーションを追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,8 @@
 class Customer < ApplicationRecord
   before_create :create_stripe_customer
+  validates :stripe_id, presence: true, unless: Proc.new { |customer| customer.new_record? }
+  validates :name, presence: true
+  validates :email, presence: true
 
   private
 
