cea53c67f1f2c5059be785ca0a5ce7ed3d521dd3
doiko123 <ayanottyz@gmail.com>
2021-06-02 22:48:22 +0900

    Skuをcreateするメソッドを追加

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -2,4 +2,17 @@ class Sku < ApplicationRecord
 
 belongs_to :product
 
+  def create_with_stripe!
+    stripe_sku = Stripe::SKU.create({
+      price: price,
+      currency: 'jpy',
+      inventory: {type: 'infinite'},
+      product: product.stripe_id
+    })
+
+    self.stripe_id = stripe_sku.id
+
+    self.save!
+    self
+  end
 end
