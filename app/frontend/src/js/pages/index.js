8b1b5717089cec94477422edd86531c68c9a67e2
doiko123 <ayanottyz@gmail.com>
2021-06-21 00:14:18 +0900

    利用規約にチェックした場合のみformをsubmitできるようにした

--- app/frontend/src/js/pages/index.js
+++ app/frontend/src/js/pages/index.js
@@ -1,1 +1,14 @@
-alert("あきまへん");
+// 利用規約に同意のチェックを入れた場合のみフォームをsubmitできるようにしている
+$(document).on("change", ".top-page #agree-check", function () {
+  // フォームのsubmitボタン
+  const submitButton = $(".top-page #submit-button");
+
+  if ($(this).prop("checked")) {
+    console.log("あああ");
+    submitButton.removeClass("disable");
+    submitButton.prop("disabled", false);
+  } else {
+    submitButton.addClass("disable");
+    submitButton.prop("disabled", true);
+  }
+});
