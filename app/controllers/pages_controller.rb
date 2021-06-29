9301924088de6194aa9c6825974036d293c95dc9
doiko123 <ayanottyz@gmail.com>
2021-06-30 01:08:58 +0900

    エラーハンドリングを追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -34,6 +34,34 @@ class PagesController < ApplicationController
 
     # 処理が正常に完了したらthanksページへ遷移
     redirect_to action: :thanks
+
+  rescue Stripe::CardError => e
+    # カードに関するエラー
+    Rails.logger.error e
+    Rails.logger.error e.class
+    Rails.logger.error e.backtrace.join("\n")
+    flash[:alert] = '【入力内容をご確認ください】カードについてエラーとなりました。入力内容がお間違いないかご確認頂き、再度エラーになる場合は別のカードでお試し下さい。'
+    redirect_to action: :index
+  rescue Stripe::InvalidRequestError => e
+    # 無効なパラメータが渡された場合のエラー（パラメータが存在しない＝未入力の場合など）
+    Rails.logger.error e
+    Rails.logger.error e.class
+    Rails.logger.error e.backtrace.join("\n")
+    flash[:alert] = '【入力内容をご確認ください】入力内容についてエラーとなりました。入力内容がお間違いないかご確認の上、修正・お進み願います。'
+    redirect_to action: :index
+  rescue Stripe::StripeError => e
+    # その他Stripeに関する接続・認証・一般的なエラー
+    Rails.logger.error e
+    Rails.logger.error e.class
+    Rails.logger.error e.backtrace.join("\n")
+    flash[:alert] = '【時間を空けてお試しください】現在決済システムにて問題が発生しておりカードをご登録いただけません。恐れ入りますが時間をあけて再度お試しください。'
+    redirect_to action: :index
+  rescue StandardError => e
+    # otomoni側のエラー
+    Rails.logger.error e
+    Rails.logger.error e.backtrace.join("\n")
+    flash[:alert] = '【時間を空けてお試しください】システムで問題が発生しておりカードをご登録いただけません。恐れ入りますが時間をあけて再度お試しください。'
+    redirect_to action: :index
   end
 
   def thanks; end
